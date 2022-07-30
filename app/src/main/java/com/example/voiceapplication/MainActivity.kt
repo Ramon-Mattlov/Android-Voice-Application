23deededa48f0e191b1748366b492cac05feacec
Ramon Mattlov <ramon_mattlov@vulong-development.ru>
2022-07-30 09:53:11 +0300

    Project initialization

--- app/src/main/java/com/example/voiceapplication/MainActivity.kt
+++ app/src/main/java/com/example/voiceapplication/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+<<<<<<< HEAD
     }
 }
+=======
+
+        val name: String = "Nikita"
+        val surname: String = "Shumskii"
+        val age: Int  = 26
+        var heigth: Double = 180.0
+
+        val output: output = findViewById(R.id.output)
+        output.text = name
+
+    }
+}/
+>>>>>>> 9be9067 (Project initialization)
